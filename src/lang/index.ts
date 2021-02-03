69ab5a573b2259d0238ddfe215387df349d2e759
Jiwon Jung <jiwonish@coinplug.com>
2021-02-03 17:18:21 +0900

    Function: initialize

---
+++ src/lang/index.ts
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+
+import { getLanguage } from '@/utils/cookies'
+
+// element-ui built-in lang
+import elementEnLocale from 'element-ui/lib/locale/lang/en'
+import elementKoLocale from 'element-ui/lib/locale/lang/ko'
+
+// User defined lang
+import enLocale from './en'
+import koLocale from './ko'
+
+Vue.use(VueI18n)
+
+const messages = {
+  en: {
+    ...enLocale,
+    ...elementEnLocale
+  },
+  ko: {
+    ...koLocale,
+    ...elementKoLocale
+  }
+}
+
+export const getLocale = () => {
+  const cookieLanguage = getLanguage()
+  if (cookieLanguage) {
+    document.documentElement.lang = cookieLanguage
+    return cookieLanguage
+  }
+
+  const language = navigator.language.toLowerCase()
+  const locales = Object.keys(messages)
+  for (const locale of locales) {
+    if (language.indexOf(locale) > -1) {
+      document.documentElement.lang = locale
+      return locale
+    }
+  }
+
+  // Default language is english
+  return 'en'
+}
+
+const i18n = new VueI18n({
+  locale: getLocale(),
+  messages
+})
+
+export default i18n
---
+++ src/views/push/index.vue
@@ -0,0 +1,19 @@
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import { handleClipboard } from '@/utils/clipboard'
+
+@Component({
+  name: 'Notice'
+})
+export default class extends Vue {
+  private handleClipboard = handleClipboard
+
+  private generateElementIconCode(symbol: string) {
+    return `<i class="el-icon-${symbol}" />`
+  }
+
+  private generateSvgIconCode(symbol: string) {
+    return `<svg-icon name="${symbol}" />`
+  }
+}
+</script>
